882394cd39883f6e1069bec8edf2904abffa015a
charon2121 <2833121040@qq.com>
2024-07-02 20:32:57 +0800

    update:初始化后端服务

---
+++ coj-backend/src/main/java/com/coj/cojbackend/CojBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coj.cojbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CojBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CojBackendApplication.class, args);
+    }
+
+}
---
+++ coj-backend/src/test/java/com/coj/cojbackend/CojBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coj.cojbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CojBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
