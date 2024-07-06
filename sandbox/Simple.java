c7170ab01885542f3770ce3db5208ccfc0906a7c
charon2121 <2833121040@qq.com>
2024-07-06 22:44:01 +0800

    feat: 初始化判题机实现方案

---
+++ sandbox/Simple.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Simple {
+    public static void main(String[] args) {
+        String userDir = System.getProperty("user.dir");
+        System.out.println("Hello World!");
+        System.out.println(userDir);
+    }
+}
