d3f8d15fc3906ee0ae2e87470fb12fca57627439
charon2121 <2833121040@qq.com>
2024-07-16 20:55:54 +0800

    feat: 添加 SpringSecurity，用于实现网站授权认证安全管理

---
+++ coj-backend/src/main/java/com/coj/cojbackend/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.coj.cojbackend.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // 禁用跨站请求伪造
+                .cors(withDefaults())  // 启用 CORS（跨域资源共享）withDefaults() 方法表示使用默认的 CORS 配置
+                .authorizeHttpRequests(auth -> auth  // 配置 HTTP 请求的授权规则
+                        .requestMatchers("/api/user").hasRole("ADMIN")
+                        .requestMatchers("/api/admin").hasRole("ADMIN")
+                        .anyRequest().permitAll()  // 所有其他请求都允许访问（不需要认证）
+                )
+                .httpBasic(withDefaults());  // 启用 HTTP Basic 认证
+
+        // 构建并返回 SecurityFilterChain 实例
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var user = User.withUsername("user")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+
+        var admin = User.withUsername("admin")
+                .password("{noop}password")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
--- coj-backend/src/main/java/com/coj/cojbackend/controller/TestController.java
+++ coj-backend/src/main/java/com/coj/cojbackend/controller/TestController.java
@@ -2,14 +2,10 @@ package com.coj.cojbackend.controller;
 
 import cn.hutool.core.lang.Snowflake;
 import cn.hutool.core.util.IdUtil;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/")
@@ -23,50 +19,17 @@ public class TestController {
     public String userId() {
         long workerId = 1;
         long datacenterId = 1;
-
         Snowflake snowflake = IdUtil.getSnowflake(workerId, datacenterId);
-
         return String.valueOf(snowflake.nextId());
     }
 
-    @GetMapping("/longpoll")
-    public String longpoll() throws JsonProcessingException {
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        Person person = new Person("coj", 18);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.writeValueAsString(person);
-    }
-}
-
-
-class Person {
-    private String name;
-    private int age;
-
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
+    @GetMapping("/user")
+    public String user() {
+        return "ok";
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
+    @GetMapping("/admin")
+    public String admin() {
+        return "ok";
     }
 }
